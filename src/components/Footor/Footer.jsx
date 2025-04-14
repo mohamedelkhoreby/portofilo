d41e6aea8fe0a2b2a3e70168cce379079b3ad9d9
mohamedelkhoreby <mohgamal518@gmail.com>
2025-04-14 13:19:00 +0200

    Fix import path casing for logo asset in Footer component

--- src/components/Footor/Footer.jsx
+++ src/components/Footor/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../Assets/logo.png";
 import InstagramIcon from "../../Assets/instagram.svg";
 import LinkedInIcon from "../../Assets/linkedin.svg";
 import GitHubIcon from "../../Assets/github.svg";
